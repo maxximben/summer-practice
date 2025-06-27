ea403975521d5f50b1767b34248aef2b8ae5fce9
maxximben <maximben888@gmail.com>
2025-06-27 19:54:44 +0600

    feat:add spaceships inplementation

---
+++ practice2025/task04/task04.cs
@@ -0,0 +1,69 @@
+﻿namespace task04
+{
+    public interface ISpaceship
+    {
+        void MoveForward();      // Движение вперед
+        void Rotate(int angle);  // Поворот на угол (градусы)
+        void Fire();             // Выстрел ракетой
+        int Speed { get; }       // Скорость корабля
+        int FirePower { get; }   // Мощность выстрела
+    }
+
+    public class Cruiser : ISpaceship
+    {
+        public int Speed { get; set; } = 50;
+        public int FirePower { get; set; } = 100;
+
+        public double X { get; set; } = 0;
+        public double Y { get; set; } = 0;
+
+        public double Angle { get; set; } = 0;
+
+        public int Rockets = 25;
+
+        public void MoveForward() 
+        { 
+            X += Speed * Math.Cos(Angle * Math.PI / 180);
+            Y += Speed * Math.Sin(Angle * Math.PI / 180);
+        }
+
+        public void Rotate(int angle)
+        {
+            Angle += angle;
+        }
+
+        public void Fire()
+        {
+            Rockets--;
+        }
+    }
+
+    public class Fighter : ISpaceship 
+    {
+        public int Speed { get; set; } = 100;
+        public int FirePower { get; set; } = 50;
+
+        public double X { get; set; } = 0;
+        public double Y { get; set; } = 0;
+
+        public double Angle { get; set; } = 0;
+
+        public int Rockets = 10;
+
+        public void MoveForward()
+        {
+            X += Speed * Math.Cos(Angle * Math.PI / 180);
+            Y += Speed * Math.Sin(Angle * Math.PI / 180);
+        }
+
+        public void Rotate(int angle)
+        {
+            Angle += angle;
+        }
+
+        public void Fire()
+        {
+            Rockets--;
+        }
+    }
+}
---
+++ practice2025/task04tests/task04tests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using task04;
+
+public class SpaceshipTests
+{
+    [Fact]
+    public void Cruiser_ShouldHaveCorrectStats()
+    {
+        ISpaceship cruiser = new Cruiser();
+        Assert.Equal(50, cruiser.Speed);
+        Assert.Equal(100, cruiser.FirePower);
+    }
+
+    [Fact]
+    public void Fighter_ShouldBeFasterThanCruiser()
+    {
+        var fighter = new Fighter();
+        var cruiser = new Cruiser();
+        Assert.True(fighter.Speed > cruiser.Speed);
+    }
+}
