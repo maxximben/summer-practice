90d6508a8a0fcd140ad6265130886ba99b67409d
maxximben <maximben888@gmail.com>
2025-06-27 15:00:12 +0600

    feat: add task01

---
+++ practice2025/task01/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (input == "" || input == null) { return false; }
+
+        string cleaned = "";
+        string reversed = "";
+
+        foreach (char c in input.ToLower())
+        {
+            if (!char.IsWhiteSpace(c) && !char.IsPunctuation(c))
+            {
+                cleaned += c;
+                reversed = c + reversed;
+            }
+        }
+
+        return cleaned == reversed;
+    }
+}
