7a039dd8808ec376487a4d28b1bb3b214488c2cc
maxximben <maximben888@gmail.com>
2025-06-28 15:46:01 +0600

    feat: add task05

---
+++ practice2025/task05tests/task05tests.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+using task05;
+//using Moq;
+
+public class TestClass
+{
+    public int PublicField;
+    private string _privateField;
+    public int Property { get; set; }
+
+    public void Method() { }
+}
+
+public class TestClass2
+{
+    public void Method(int a, int b) { }
+}
+
+[Serializable]
+public class AttributedClass { }
+
+public class ClassAnalyzerTests
+{
+    [Fact]
+    public void GetPublicMethods_ReturnsCorrectMethods()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var methods = analyzer.GetPublicMethods();
+
+        Assert.Contains("Method", methods);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPrivateFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+    
+        Assert.Contains("_privateField", fields);
+    }
+    
+    
+    [Fact]
+    public void HasAttribute_ReturnsTrue()
+    {
+        var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+        var result = analyzer.HasAttribute<SerializableAttribute>();
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void HasAttribute_ReturnsFalse()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var result = analyzer.HasAttribute<SerializableAttribute>();
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void GetProperties_ReturnsCorrectProperties()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var properties = analyzer.GetProperties();
+        Assert.Contains("Property", properties);
+    }
+    
+    [Fact]
+    public void GetMethodParams_ReturnsCorrectParameters()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass2));
+        var parameters = analyzer.GetMethodParams("Method");
+        
+        Assert.Contains("a", parameters);
+        Assert.Contains("b", parameters);
+    }
+}
