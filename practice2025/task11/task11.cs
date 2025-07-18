43234e826c31723ce824645920e439619b745c9b
maxximben <maximben888@gmail.com>
2025-07-18 13:38:37 +0600

    feat: add task11

---
+++ practice2025/task11/task11.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Reflection;
+
+namespace task11;
+
+
+public interface ICalculator
+{
+    int Add(int a, int b);
+    int Minus(int a, int b);
+    int Mul(int a, int b);
+    int Div(int a, int b);
+}
+
+public class CalculatorGenerator
+{
+    public static ICalculator CreateCalc(string code)
+    {
+        var references = new MetadataReference[]
+        {
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(ICalculator).Assembly.Location),
+        };
+
+        var compile = CSharpCompilation.Create("Calc", new[] { CSharpSyntaxTree.ParseText(code) }, references,
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        var memoryStream = new MemoryStream();
+        compile.Emit(memoryStream);
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        var assembly = Assembly.Load(memoryStream.ToArray());
+        
+        var calculatorType = assembly.GetType("Calculator");
+        if (calculatorType is null)
+        {
+            throw new ArgumentNullException();
+        }
+        
+        var calc = Activator.CreateInstance(calculatorType);
+        if (calc is null)
+        {
+            throw new ArgumentNullException();
+        }
+        
+        return (ICalculator) calc;
+    }
+}
---
+++ practice2025/task11tests/task11tests.cs
@@ -0,0 +1,46 @@
+﻿using task11;
+
+namespace task11tests;
+
+public class task11tests
+{
+    private static string code = @"
+
+    using task11;
+    public class Calculator : ICalculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Minus(int a, int b) => a - b;
+        public int Mul(int a, int b) => a * b;
+        public int Div(int a, int b) => a / b;
+    }";
+    
+    [Fact]
+    public void AddReturnsCorrectValue()
+    {
+        var calc = CalculatorGenerator.CreateCalc(code);
+        Assert.Equal(5, calc.Add(2, 3));
+    }
+
+    [Fact]
+    public void MinusReturnsCorrectValue()
+    {
+        var calc = CalculatorGenerator.CreateCalc(code);
+        Assert.Equal(5, calc.Minus(7, 2));
+    }
+
+    [Fact]
+    public void MulReturnsCorrectValue()
+    {
+        var calc = CalculatorGenerator.CreateCalc(code);
+        Assert.Equal(12, calc.Mul(3, 4));
+    }
+
+    [Fact]
+    public void DivReturnsCorrectValue()
+    {
+        var calc = CalculatorGenerator.CreateCalc(code);
+        Assert.Equal(4, calc.Div(12, 3));
+    }
+}
+
